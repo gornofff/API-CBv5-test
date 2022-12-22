de55251c3c4306ec13fdb27c219918e6d6f1b48f
dumpdusty <dumpdusty@gmail.com>
2022-12-21 21:28:34 -0800

    mocha-supertest-chai installation

---
+++ .mocharc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    require: ['@babel/register'],
+    spec: 'path to tests directory',
+    exclude: 'path to files to be excluded from test run',
+    file: `path to global hook`,
+}
