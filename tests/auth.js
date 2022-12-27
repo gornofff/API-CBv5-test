02ef2069622416f9a2e4307e13283f265394f7cc
dumpdusty <dumpdusty@gmail.com>
2022-12-26 21:43:32 -0800

    add hooks

--- tests/auth.js
+++ tests/auth.js
@@ -1,58 +1,44 @@
 import supertest from 'supertest'
 import {expect} from 'chai'
-import 'dotenv/config'
 
 describe('Authentication', () => {
     describe('Login with valid credentials', () => {
         let res
-        it('Check the status code', async () => {
+        before(async () => {
             res = await supertest(process.env.BASE_URL)
                 .post('user/login')
                 .send({email: process.env.EMAIL, password: process.env.PASS})
+        })
 
+        it('Check the status code', async () => {
             expect(res.statusCode).to.eq(200)
         })
 
         it('Check the status message', async () => {
-            res = await supertest(process.env.BASE_URL)
-                .post('user/login')
-                .send({email: process.env.EMAIL, password: process.env.PASS})
-
             expect(res.body.message).to.eq('Auth success')
         })
 
         it('Check the response has token', async () => {
-            res = await supertest(process.env.BASE_URL)
-                .post('user/login')
-                .send({email: process.env.EMAIL, password: process.env.PASS})
-
             expect(res.body.payload.token).to.not.be.undefined
         })
     })
 
     describe('Login with invalid credentials', () => {
         let res
-        it('Check the status code', async () => {
+        before(async () => {
             res = await supertest(process.env.BASE_URL)
                 .post('user/login')
                 .send({email: 'invalid@pirate.com', password: 'invalid'})
-
+        })
+        it('Check the status code', async () => {
             expect(res.statusCode).to.eq(400)
         })
 
         it('Check the status message', async () => {
-            res = await supertest(process.env.BASE_URL)
-                .post('user/login')
-                .send({email: 'invalid@pirate.com', password: 'invalid'})
-
             expect(res.body.message).to.eq('Auth failed')
         })
 
         it('Check the response succes status', async () => {
-            res = await supertest(process.env.BASE_URL)
-                .post('user/login')
-                .send({email: 'invalid@pirate.com', password: 'invalid'})
-
             expect(res.body.success).to.be.false
         })
     })
