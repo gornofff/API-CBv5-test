dac6fb35e0e432efe59f1e9cd275480da00bdf56
dumpdusty <dumpdusty@gmail.com>
2022-12-26 20:02:15 -0800

    add env variables

--- tests/auth.js
+++ tests/auth.js
@@ -1,56 +1,59 @@
 import supertest from 'supertest'
-import { expect } from 'chai'
+import {expect} from 'chai'
+import 'dotenv/config'
 
-describe.skip('Login with valid credentials', () => {
-    let res
-    it('Check the status code', async () => {
-        res = await supertest('https://clientbase-server.herokuapp.com/v5/')
-            .post('user/login')
-            .send({email: 'jacksparrow@pirate.com', password: 'Pirate666!'})
+describe('Authentication', () => {
+    describe('Login with valid credentials', () => {
+        let res
+        it('Check the status code', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: process.env.PASS})
 
-        expect(res.statusCode).to.eq(200)
-    });
+            expect(res.statusCode).to.eq(200)
+        })
 
-    it('Check the status message', async () => {
-        res = await supertest('https://clientbase-server.herokuapp.com/v5/')
-            .post('user/login')
-            .send({email: 'jacksparrow@pirate.com', password: 'Pirate666!'})
+        it('Check the status message', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: process.env.PASS})
 
-        expect(res.body.message).to.eq('Auth success')
-    });
+            expect(res.body.message).to.eq('Auth success')
+        })
 
-    it('Check the response has token', async () => {
-        res = await supertest('https://clientbase-server.herokuapp.com/v5/')
-            .post('user/login')
-            .send({email: 'jacksparrow@pirate.com', password: 'Pirate666!'})
+        it('Check the response has token', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: process.env.PASS})
 
-        expect(res.body.payload.token).to.not.be.undefined
-    });
+            expect(res.body.payload.token).to.not.be.undefined
+        })
+    })
+
+    describe('Login with invalid credentials', () => {
+        let res
+        it('Check the status code', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: 'invalid@pirate.com', password: 'invalid'})
+
+            expect(res.statusCode).to.eq(400)
+        })
+
+        it('Check the status message', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: 'invalid@pirate.com', password: 'invalid'})
+
+            expect(res.body.message).to.eq('Auth failed')
+        })
+
+        it('Check the response succes status', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: 'invalid@pirate.com', password: 'invalid'})
+
+            expect(res.body.success).to.be.false
+        })
+    })
 })
-
-describe('Login with invalid credentials', () => {
-    let res
-    it('Check the status code', async() => {
-        res = await supertest('https://clientbase-server.herokuapp.com/v5/')
-            .post('user/login')
-            .send({email: 'invalid@pirate.com', password: 'invalid'})
-
-        expect(res.statusCode).to.eq(400)
-    });
-
-    it('Check the status message', async() => {
-        res = await supertest('https://clientbase-server.herokuapp.com/v5/')
-            .post('user/login')
-            .send({email: 'invalid@pirate.com', password: 'invalid'})
-
-        expect(res.body.message).to.eq('Auth failed')
-    });
-
-    it('Check the response succes status', async() => {
-        res = await supertest('https://clientbase-server.herokuapp.com/v5/')
-            .post('user/login')
-            .send({email: 'invalid@pirate.com', password: 'invalid'})
-
-        expect(res.body.success).to.be.false
-    });
-});
