804b39a1bd7d535f71caa005582f302ad7cb980c
dumpdusty <dumpdusty@gmail.com>
2023-01-03 12:07:13 -0800

    update auth.spec.js

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,22 +1,30 @@
 import {expect} from 'chai'
-import * as authHelper from '../helpers/auth-helper.js'
+import supertest from 'supertest'
+import 'dotenv/config'
+
+
 
 describe('Authentication Positive', () => {
     describe('Login with valid credentials', () => {
         let res
-        before(async () => {
-            res = await authHelper.login(process.env.EMAIL, process.env.PASSWORD)
-        })
-
         it('Check the status code', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
             expect(res.statusCode).to.eq(200)
         })
 
         it('Check the status message', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
             expect(res.body.message).to.eq('Auth success')
         })
 
         it('Check the response has token', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
             expect(res.body.payload.token).to.not.be.undefined
         })
     })
@@ -24,55 +32,53 @@ describe('Authentication Positive', () => {
 describe('Authentication Negative', () => {
     describe('Login with invalid email', () => {
         let res
-        before(async () => {
-            res = await authHelper.login('invalid@pirate.com', process.env.PASSWORD)
-        })
+
         it('Check the status code', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: 'invalid@pirate.com', password: process.env.PASSWORD})
             expect(res.statusCode).to.eq(400)
         })
 
         it('Check the status message', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: 'invalid@pirate.com', password: process.env.PASSWORD})
             expect(res.body.message).to.eq('Auth failed')
-        })
-
-        it('Check the response success status', async () => {
-            expect(res.body.success).to.be.false
         })
     })
 
     describe('Login with invalid password', () => {
         let res
-        before(async () => {
-            res = await authHelper.login(process.env.EMAIL, 'invalid')
-        })
         it('Check the status code', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: 'invalid'})
             expect(res.statusCode).to.eq(400)
         })
 
         it('Check the status message', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: process.env.EMAIL, password: 'invalid'})
             expect(res.body.message).to.eq('Auth failed')
-        })
-
-        it('Check the response success status', async () => {
-            expect(res.body.success).to.be.false
         })
     })
 
     describe('Login with empty fields', () => {
         let res
-        before(async () => {
-            res = await authHelper.login('', '')
-        })
         it('Check the status code', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: '', password: ''})
             expect(res.statusCode).to.eq(400)
         })
 
         it('Check the status message', async () => {
+            res = await supertest(process.env.BASE_URL)
+                .post('user/login')
+                .send({email: '', password: ''})
             expect(res.body.message).to.eq('Auth failed')
-        })
-
-        it('Check the response success status', async () => {
-            expect(res.body.success).to.be.false
         })
     })
 })
