05144371c05d587308763dc4a5d4a3393c8a296b
dumpdusty <dumpdusty@gmail.com>
2023-01-18 16:56:29 -0800

    add negative tests for register.spec.js

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -25,3 +25,36 @@ describe('Create a user with valid credentials', () => {
     })
 });
 
+describe('User registration negative', () => {
+    describe('create a user without password', () => {
+        let res
+
+        before(async()=>{
+            res = await register(chance.first(), chance.last(), chance.email(), '')
+        })
+
+        it('check response status code',  () => {
+            expect(res.statusCode).to.eq(400)
+        })
+
+        it('check response message',  () => {
+            expect(res.body.message).to.eq('Wrong password format')
+        })
+    })
+
+    describe('create a user with existing email', () => {
+        let res
+
+        before(async()=>{
+            res = await register(chance.first(), chance.last(), process.env.EMAIL, process.env.PASSWORD)
+        })
+
+        it('check response status code', () => {
+            expect(res.statusCode).to.eq(409)
+        })
+
+        it('check response message',  () => {
+            expect(res.body.message).to.eq('User with this e-mail exists')
+        })
+    })
+})
