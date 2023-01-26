138305b53284b0f2603addfe9d5d16fa6e285e6f
dumpdusty <dumpdusty@gmail.com>
2023-01-25 17:57:53 -0800

    add reporter

--- .mocharc.js
+++ .mocharc.js
@@ -3,4 +3,7 @@ module.exports = {
     spec: 'tests/**/*.js',
     exclude: 'tests/example.spec.js',
     file: `setup/global-hooks.js`,
+    reporter: 'mochawesome',
+    'reporter-options': 'json=false,reportFilename=MyReport,reportDir=Reports'
+
 }
